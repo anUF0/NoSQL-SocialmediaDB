3582658d6b972539963065b8f5c64567cd3a7096
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-09-25 14:34:50 +1000

    redid main index and config

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const cwd = process.cwd();
-app = express();
 
-//Project 14 was the name of the file the local repo was in
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes('Project 14') ? cwd.split('Project 14')[1] : cwd;
 
 app.use(express.urlencoded({ extended: true }));
